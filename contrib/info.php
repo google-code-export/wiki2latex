0b759c9146eb1b0a296d11fd87635877ceb28b4a
Hans-Georg Kluge <hansgeorg.kluge@googlemail.com>
2010-08-27 17:52:17 +0200

    Adding polish translation and contrib/info.php (thx to: Szymon Tarnowski)

---
+++ contrib/info.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Adds some information at the end of an article (as LaTeX-comment)
+ */
+ 
+ if ( !defined('MEDIAWIKI') ) {
+	$msg  = 'To install Wiki2LaTeX, put the following line in LocalSettings.php:<br/>';
+	$msg .= '<tt>require_once( $IP."/extensions/path_to_Wiki2LaTeX_files/wiki2latex.php" );</tt>';
+	echo $msg;
+	exit( 1 );
+}
+ 
+$wgHooks['w2lFinish'][]               = 'w2lInfoHook';
+
+function w2lInfoHook(&$parser, &$text)
+{
+    $info = "\n";
+    $info = $info.'%'."\n";
+    $info = $info.'% Wiki2LaTeX infobox'."\n";
+    $info = $info.'% source article: '.($parser->mTitle)."\n";
+    $info = $info.'% generated at: '.date('H:i:s d-m-Y')."\n";
+    $info = $info.'%'."\n";
+    $info = $info."\n";
+
+    /*final copy*/    
+    $text = $info.$text;
+    
+    return true;
+}
+
+
